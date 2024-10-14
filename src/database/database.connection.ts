6b764cb3fc5178cebba20c6a34ffe1ac70f22a6a
Danil Gonzalez <danilo.j.gonzalez@gmail.com>
2024-10-14 15:08:58 -0600

    comment the code

--- src/database/database.connection.ts
+++ src/database/database.connection.ts
@@ -10,6 +10,20 @@ import {
     NODE_ENV,
 } from "../config/configuration";
 
+/**
+ * Initializes a new Sequelize instance to connect to the database.
+ *
+ * @remarks
+ * This instance uses environment variables for database configuration such as
+ * database name, username, password, host, port, and timezone. The logging
+ * option is enabled only in development mode.
+ *
+ * - If in production mode, SSL is required for the connection, with SSL
+ *   certificate validation disabled.
+ * - In development mode, logging is enabled to display SQL queries in the console.
+ *
+ * @constant
+ */
 export const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
     dialect: DB_DRIVER,
     host: DB_HOST,
@@ -25,6 +39,17 @@ export const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
     },
 });
 
+/**
+ * Opens a connection to the database and retries in case of failure.
+ *
+ * @remarks
+ * This function attempts to authenticate the Sequelize connection to the database.
+ * If the connection fails, it retries up to 10 times, waiting 5 seconds between retries.
+ *
+ * @throws Will throw an error if the connection fails after 10 retries.
+ *
+ * @returns A promise that resolves when the connection is successfully established.
+ */
 export const openDBConnection = async () => {
     let retries = 10;
 
@@ -41,6 +66,15 @@ export const openDBConnection = async () => {
     }
 };
 
+/**
+ * Drops all tables and closes the database connection.
+ *
+ * @remarks
+ * This function drops all tables in the database (in cascade mode) and then
+ * closes the Sequelize connection.
+ *
+ * @returns A promise that resolves when the connection is closed and tables are dropped.
+ */
 export const dropAndCloseDBConnection = async () => {
     await sequelize
         .drop({ cascade: true })
